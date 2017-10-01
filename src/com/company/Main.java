694d1563086a528f798b10dd0f5bd35ea428f75a
aleoley <alex23oleynik@list.ru>
2017-10-01 22:13:05 +0300

    output array of strings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world!");
+        print(args);
     }
+    public static void print(String[] args)
+    {
+        for(int i=0; i<args.length; i++)
+        {
+            System.out.println(args[i]);
+        }
+    }
+
 }
